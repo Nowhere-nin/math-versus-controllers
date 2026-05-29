6137156d364a749f1307743ec6ec60d1e458d8c8
Michael Sandoval <michael.sandovalm20@gmail.com>
2026-05-29 09:17:08 -0400

    Otro

---
+++ vite.config.ts
@@ -0,0 +1,35 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
+
+// https://vite.dev/config/
+export default defineConfig({
+  base: '/math-versus-controllers/',
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png'],
+      manifest: {
+        name: 'Control Matemáticas',
+        short_name: 'ControlMate',
+        description: 'Mando a distancia para el juego de Tira y Afloja Matemático',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone', // Esto oculta la barra del navegador
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
+})
